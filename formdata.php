24eacd1cb5bec17cb6a2e35b2ff33845a6c83453
itsmordecai <mordecai.kipngetich@gmail.com>
2022-02-28 10:46:03 +0300

    save hospital data to database

--- formdata.php
+++ formdata.php
@@ -1,7 +1,16 @@
 <?php
 
+$link = mysqli_connect("localhost", "root", "", "vivaldi");
+
+if ($link == true) {
+  echo "Connection was successful <br>";
+} else {
+  echo "Error connecting " . mysqli_connect_error($link) . "<br>";
+}
+
+
 if (isset($_POST["submit"])) {
-  
+
   // Basic Info
   $doctorFName = $_POST["doctorFName"];
   $doctorLName = $_POST["doctorLName"];
@@ -13,7 +22,6 @@ if (isset($_POST["submit"])) {
   $patientLName = $_POST["patientLName"];
   $patientDOB = $_POST["patientDOB"];
   $gender = $_POST["gender"];
-  $ageGroup = $_POST["ageGroup"];
   $patientEmail = $_POST["patientEmail"];
   $patientPhoneNumber = $_POST["patientPhoneNumber"];
   $preferredContactMethod = $_POST["preferredContactMethod"];
@@ -27,46 +35,48 @@ if (isset($_POST["submit"])) {
 
   // Agreement Info
   $tsandcs = $_POST["tsandcs"];
-  $todaysDate = $_POST["todaysDate"];
+  $agreementDate = $_POST["todaysDate"];
   $signature = $_POST["signature"];
-  
-  echo "<h4>Basic Information</h4>";
-  echo "Patient booked Dr. $doctorLName $doctorFName on $admissionDate for $plannedProcedure <br>";
 
-  echo "<hr>";
+  $sql = "INSERT INTO `hospital`(`doctorFName`, `doctorLName`, `admissionDate`, `plannedProcedure`, `patientFName`, `patientLName`, `patientDOB`, `gender`, `patientEmail`, `patientPhoneNumber`, `preferredContactMethod`, `kinFName`, `kinLName`, `kinRelationship`, `kinEmail`, `kinPhoneNumber`, `tsandcs`, `agreementDate`, `signature`) VALUES ('$doctorFName','$doctorLName','$admissionDate','$plannedProcedure','$patientFName','$patientLName','$patientDOB','$gender','$patientEmail','$patientPhoneNumber','$preferredContactMethod','$kinFName','$kinLName','$kinRelationship','$kinEmail','$kinPhoneNumber','$tsandcs','$agreementDate','$signature')";
+  $result = mysqli_query($link, $sql);
 
-  echo "<h4>Patient Information</h4>";
-  echo "Name: $patientLName $patientFName <br>";
-  echo "DOB: $patientDOB <br>";
-  echo "Gender: $gender <br>";
-  if($ageGroup == "adult"){
-    echo "Age Group: Over 18 <br>";
-  } else if ($ageGroup == "minor"){
-    echo "Age Group: Under 18 <br>";
+  if ($result) {
+    echo "Data has been added successfully <br>";
   } else {
-    echo "Age Group: Undefined <br>";
+    echo "Error adding this record $sql " . mysqli_error($link) . "<br>";
   }
-  echo "Email: $patientEmail <br>";
-  echo "Phone: $patientPhoneNumber <br>";
-  echo "Preferred Contact Method: $preferredContactMethod <br>";
 
-  echo "<hr>";
+  // echo "<h4>Basic Information</h4>";
+  // echo "Patient booked Dr. $doctorLName $doctorFName on $admissionDate for $plannedProcedure <br>";
 
-  echo "<h4>Next of Kin/Contact Person Information</h4>";
-  echo "Name: $kinLName $kinFName <br>";
-  echo "Relationship To Patient: $kinRelationship <br>";
-  echo "Email: $kinEmail <br>";
-  echo "Phone: $kinPhoneNumber <br>";
+  // echo "<hr>";
 
-  echo "<hr>";
+  // echo "<h4>Patient Information</h4>";
+  // echo "Name: $patientLName $patientFName <br>";
+  // echo "DOB: $patientDOB <br>";
+  // echo "Gender: $gender <br>";
+  // echo "Email: $patientEmail <br>";
+  // echo "Phone: $patientPhoneNumber <br>";
+  // echo "Preferred Contact Method: $preferredContactMethod <br>";
 
-  echo "<h4>Agreement</h4>";
-  if ($tsandcs == "agreed"){
-    echo "Patient agreed to the Terms and Conditions <br>";
-  } else {
-    echo "Patient did not agree to the Terms and Conditions <br>";
-  }
-  echo "Agreement Date: $todaysDate <br>";
-  echo "Signature: $signature <br>";
+  // echo "<hr>";
+
+  // echo "<h4>Next of Kin/Contact Person Information</h4>";
+  // echo "Name: $kinLName $kinFName <br>";
+  // echo "Relationship To Patient: $kinRelationship <br>";
+  // echo "Email: $kinEmail <br>";
+  // echo "Phone: $kinPhoneNumber <br>";
+
+  // echo "<hr>";
+
+  // echo "<h4>Agreement</h4>";
+  // if ($tsandcs == "agreed"){
+  //   echo "Patient agreed to the Terms and Conditions <br>";
+  // } else {
+  //   echo "Patient did not agree to the Terms and Conditions <br>";
+  // }
+  // echo "Agreement Date: $todaysDate <br>";
+  // echo "Signature: $signature <br>";
 
 }
