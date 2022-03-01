52145858c0b3497cb834755472f9a2d9df76add8
itsmordecai <mordecai.kipngetich@gmail.com>
2022-03-01 12:33:24 +0300

    simplify input data and make form more responsive

--- formdata.php
+++ formdata.php
@@ -1,25 +1,16 @@
 <?php
 
-$link = mysqli_connect("localhost", "root", "", "vivaldi");
-
-if ($link == true) {
-  echo "Connection was successful <br>";
-} else {
-  echo "Error connecting " . mysqli_connect_error($link) . "<br>";
-}
-
+include "connect.php";
 
 if (isset($_POST["submit"])) {
 
   // Basic Info
-  $doctorFName = $_POST["doctorFName"];
-  $doctorLName = $_POST["doctorLName"];
+  $doctorName = $_POST["doctorName"];
   $admissionDate = $_POST["admissionDate"];
   $plannedProcedure = $_POST["plannedProcedure"];
 
   // Patient Info
-  $patientFName = $_POST["patientFName"];
-  $patientLName = $_POST["patientLName"];
+  $patientName = $_POST["patientName"];
   $patientDOB = $_POST["patientDOB"];
   $gender = $_POST["gender"];
   $patientEmail = $_POST["patientEmail"];
@@ -27,23 +18,22 @@ if (isset($_POST["submit"])) {
   $preferredContactMethod = $_POST["preferredContactMethod"];
 
   // Kin Info
-  $kinFName = $_POST["kinFName"];
-  $kinLName = $_POST["kinLName"];
+  $kinName = $_POST["kinName"];
   $kinRelationship = $_POST["kinRelationship"];
   $kinEmail = $_POST["kinEmail"];
   $kinPhoneNumber = $_POST["kinPhoneNumber"];
 
   // Agreement Info
   $tsandcs = $_POST["tsandcs"];
-  $agreementDate = $_POST["todaysDate"];
   $signature = $_POST["signature"];
 
-  $sql = "INSERT INTO `hospital`(`doctorFName`, `doctorLName`, `admissionDate`, `plannedProcedure`, `patientFName`, `patientLName`, `patientDOB`, `gender`, `patientEmail`, `patientPhoneNumber`, `preferredContactMethod`, `kinFName`, `kinLName`, `kinRelationship`, `kinEmail`, `kinPhoneNumber`, `tsandcs`, `agreementDate`, `signature`) VALUES ('$doctorFName','$doctorLName','$admissionDate','$plannedProcedure','$patientFName','$patientLName','$patientDOB','$gender','$patientEmail','$patientPhoneNumber','$preferredContactMethod','$kinFName','$kinLName','$kinRelationship','$kinEmail','$kinPhoneNumber','$tsandcs','$agreementDate','$signature')";
+  $sql = "INSERT INTO `hospital`(`doctorName`, `admissionDate`, `plannedProcedure`, `patientName`, `patientDOB`, `gender`, `patientEmail`, `patientPhoneNumber`, `preferredContactMethod`, `kinName`, `kinRelationship`, `kinEmail`, `kinPhoneNumber`, `tsandcs`, `signature`) VALUES ('$doctorName','$admissionDate','$plannedProcedure','$patientName','$patientDOB','$gender','$patientEmail','$patientPhoneNumber','$preferredContactMethod','$kinName','$kinRelationship','$kinEmail','$kinPhoneNumber','$tsandcs','$signature')";
 
   $result = mysqli_query($link, $sql);
 
   if ($result) {
     echo "Data has been added successfully <br>";
+    header("location: displaydata.php");
   } else {
     echo "Error adding this record $sql " . mysqli_error($link) . "<br>";
   }
